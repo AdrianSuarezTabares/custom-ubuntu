c2481e4f57e0805fac6a72aa33f31913ccfb2e85
ASuárezTabares <91555979+AdrianSuarezTabares@users.noreply.github.com>
2025-02-28 08:29:18 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 # Cabiar el idioma a español, lanza un gui
 dpkg-reconfigure locales
 
-# Desinstar snaps
+# Desinstar snaps, pesados y lentos
 apt autoremove --purge snapd
 apt-mark hold snapd
 apt install gnome-software --no-install-recommends
@@ -23,7 +23,7 @@ apt install flatpak
 apt install gnome-software-plugin-flatpak gnome-software
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# Instalar aplicaciones
+## Instalar aplicaciones
 # firefox
 add-apt-repository ppa:mozillateam/ppa
 apt update
@@ -40,9 +40,11 @@ sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable m
 rm microsoft.gpg
 apt update && apt install microsoft-edge-stable
 
-# Instalar thunderbird
+# thunderbird
 flatpak install flathub org.mozilla.Thunderbird 
 apt install gnome-shell-extension-manager
+
+# libre office
 
 
 # instalar global protect
@@ -50,11 +52,12 @@ add-apt-repository ppa:yuezk/globalprotect-openconnect
 apt-get update
 apt-get install globalprotect-openconnect
 
-# instalar trendmicro
+# descargar trendmicro
 wget "https://files.trendmicro.com/products/deepsecurity/en/20.0/Manager-Linux-20.0.1017.x64.sh"
-# aplica la configuración 
-# sudo apt-get install dconf-cli
-# cp gnome-config.conf /etc/skel/.config/dconf/user
+# descargar vmware horizon
+wget https://download3.omnissa.com/software/CART25FQ4_LIN64_DebPkg_2412/Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.deb
+dpkg -i Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.deb
+rm Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.deb
 
 ## Cambia el branding del sistema 
 # Cambia los logos por los de inatux
@@ -62,19 +65,16 @@ cp images/ubuntu-logo.png /usr/share/plymouth/ubuntu-logo.png
 cp images/watermark.png /usr/share/plymouth/themes/spinner/watermark.png
 cp images/bgrt-fallback.png /usr/share/plymouth/themes/spinner/bgrt-fallback.png
 cp images/inatux.svg /usr/share/icons/Yaru/scalable/actions/view-app-grid-ubuntu-symbolic.svg
+
 # Cambia el forndo de pantalla
 rm /usr/share/backgrounds/*
-
 cp images/Wallpaper.jpg /usr/share/33421
 cp images/inatux-logo.png /usr/share/plymouth/
+
 # Aplica la configuración de escritorio
 tar -xzvf dconf.tar.gz dconf/   
 cp -r dconf /etc/
 dconf update
-
-# Copia un esqueleto para el home de nuevos usuarios 
-tar -xzvf skel.tar.gz skel/
-cp -r skel/ /etc/
 
 # Copia un esqueleto para el home de nuevos usuarios 
 tar -xzvf skel.tar.gz skel/
